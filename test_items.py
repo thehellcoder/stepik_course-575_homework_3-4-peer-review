4ea65b423ef3c19bbb13dcf60ea363c9c3f3c1f5
thehellcoder <the.hell.coder@gmail.com>
2019-09-03 07:47:25 +0300

    cosmetic change: add assertion message

--- test_items.py
+++ test_items.py
@@ -7,6 +7,6 @@ URL = 'http://selenium1py.pythonanywhere.com/catalogue/kingpin_186/'
 def test_guest_see_add_to_basket_button(browser):
     browser.get(URL)
     btn = browser.find_element_by_css_selector('.btn-add-to-basket')
-    assert btn.is_enabled()
+    assert btn.is_enabled(), '"Add to Basket" button is invisible for guest'
     print('\n[DEBUG] visited URL: {}'.format(browser.current_url))
     print('[DEBUG] found button with text: {}'.format(btn.text))
